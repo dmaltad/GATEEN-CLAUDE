fd93bc8ac84d4c5c274151e4b93c27fade561d01
dmaltad <daniel-malta2@hotmail.com>
2026-05-28 14:58:56 -0300

    ababa

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.utils.http import url_has_allowed_host_and_scheme
 
-from .models import Address, Pet
+from .models import Address, Pet, User
 from .forms import ProfileForm, AddressForm, PetForm
 
 
